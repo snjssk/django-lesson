02e36f5a9c297ea017b020ba029b0bb472caddb0
snjssk <snjssk0403@gmail.com>
2020-09-10 23:53:00 +0900

    update templates

--- lesson/urls.py
+++ lesson/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('temp/', views.temp, name='temp'),
+    path('list/', views.list, name='list'),
+    path('iftag/', views.iftag, name='iftag'),
 ]
--- lesson/views.py
+++ lesson/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Book
+
+import random
+
 
 def index(request):
     return HttpResponse('hello world')
@@ -12,3 +16,20 @@ def temp(request):
         'msg': 'hello world'
     }
     return render(request, 'lesson/temp.html', context)
+
+
+def list(request):
+    # all()は全件
+    books = Book.objects.all()
+    return render(request, 'lesson/list.html', {
+        'books': books
+    })
+
+
+def iftag(request):
+    return render(request, 'lesson/iftag.html', {
+        'random': random.randint(0, 100),
+        'flag': True,
+        'weeks': ['月', '火', '水', '木', '金', '土', '日'],
+        'empties': []
+    })
